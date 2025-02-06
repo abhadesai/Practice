1dffe7ebb171d4912b56f2209bcf41d696d95e59
Abha Desai <desaiabha3011@gmail.com>
2025-02-06 12:57:51 -0500

    Updated with correct solution

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -3,8 +3,21 @@ from typing import List
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+
+        duplicatesHashSet = set()
+
+        for num in nums:
+            if num not in duplicatesHashSet:
+                duplicatesHashSet.add(num)
+            else:
+                return True
         return False
+
+        #BruteForce - Array is not sorted
+
+        """for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i] == nums[j]):
+                    return True
+        return False"""
+         
